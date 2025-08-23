15b23ea7ee720a22061929d659d5c6af09219885
justin <veridiux@gmail.com>
2025-08-22 20:02:54 -0500

    update: 2025-08-22 20:02:54

--- install/install.sh
+++ install/install.sh
@@ -5,8 +5,10 @@ case "$distro" in
         echo "[*] Linking Arch configs..."
         ln -sf ~/dotfiles/distro/arch/config/hypr ~/.config/hypr
         ln -sf ~/dotfiles/distro/arch/config/waybar ~/.config/waybar
-	ls -sf ~/dotfiles/distro/arch/config/alacritty ~/.config/alacritty
+	ln -sf ~/dotfiles/distro/arch/config/alacritty ~/.config/alacritty
+	ln -sf ~/dotfiles/distro/arch/config/Kvantum ~/.config/Kvantum
 	ln -s ~/dotfiles/shells/.zshrc ~/.zshrc
+	ln -s ~/dotfiles/distro/arch/config/kdeglobals ~/.config/
 
         ;;
     debian|ubuntu)
