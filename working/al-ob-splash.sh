daefb39cac406682b5d100774f4f6e8586ae621b
Justin <veridiux@gmail.com>
2026-08-14 12:23:12 -0500

    update: 2026-08-14 12:23:12

---
+++ working/al-ob-splash.sh
@@ -0,0 +1,5 @@
+┌─[justin@E1-Arch] // ~
+└─[❯] sudo cp ~/Pictures/splash-arch-ob.bmp /usr/share/systemd/bootctl/splash-arch.bmp
+[sudo] password for justin: 
+┌─[justin@E1-Arch] // ~
+└─[❯] sudo mkinitcpio -P 
