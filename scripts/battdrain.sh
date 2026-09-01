2a0075ea89463958b072b2954c7ec0d0e2ffcc4b
justin <veridiux@gmail.com>
2026-09-01 06:43:22 -0500

    update: 2026-09-01 06:43:22

--- scripts/battdrain.sh
+++ scripts/battdrain.sh
@@ -1,1 +1,26 @@
-watch -n1 'awk "BEGIN { c=$(cat /sys/class/power_supply/BAT1/current_now); v=$(cat /sys/class/power_supply/BAT1/voltage_now); printf \"%.2f W\n\", (c*v)/1000000000000 }"'
+#!/bin/bash
+
+watch -n5 '
+charge=$(cat /sys/class/power_supply/BAT1/charge_now)
+current=$(cat /sys/class/power_supply/BAT1/current_now)
+voltage=$(cat /sys/class/power_supply/BAT1/voltage_now)
+capacity=$(cat /sys/class/power_supply/BAT1/capacity)
+status=$(cat /sys/class/power_supply/BAT1/status)
+
+awk -v c="$charge" -v i="$current" -v v="$voltage" \
+    -v cap="$capacity" -v status="$status" "BEGIN {
+        power=(i*v)/1000000000000
+
+        if (i > 0) {
+            hours=c/i
+            h=int(hours)
+            m=int((hours*60)%60)
+            time=sprintf(\"%02d:%02d\", h, m)
+        } else {
+            time=\"--:--\"
+        }
+
+        printf \"%s | %d%% | %.2f W | %s remaining\\n\",
+            status, cap, power, time
+    }"
+'
